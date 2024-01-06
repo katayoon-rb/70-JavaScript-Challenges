1047a8b2e4f7ab90cf5e1f5128d6040377df77a2
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-01-06 12:13:50 +0330

    Add files via upload

---
+++ 06 - Time & Space Complexity/08 max-subarray-quadratic.js
@@ -0,0 +1,11 @@
+function maxSubarraySum(arr, k) {
+  let maxSum = 0
+  for (let i = 0; i <= arr.length - k; i++) {
+    let currentSum = 0
+    for (let j = i; j < i + k; j++) {
+      currentSum += arr[j]
+    }
+    maxSum = Math.max(maxSum, currentSum)
+  }
+  return maxSum
+}

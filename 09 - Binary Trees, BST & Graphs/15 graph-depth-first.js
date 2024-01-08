0c58b115a2d9a6595f45166275e7981b85078a2b
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-01-08 19:24:16 +0330

    Add files via upload

---
+++ 09 - Binary Trees, BST & Graphs/15 graph-depth-first.js
@@ -0,0 +1,25 @@
+import Stack from './stack'
+
+function depthFirstTraversal(graph, startingVertex) {
+  if (!graph.adjacencyList[startingVertex]) { return [] }
+
+  const visited = {}
+  const stack = new Stack()
+  const result = []
+
+  stack.push(startingVertex)
+  visited[startingVertex] = true
+
+  while (!stack.isEmpty()) {
+    const currentVertex = stack.pop()
+    result.push(currentVertex)
+
+    graph.adjacencyList[currentVertex].forEach((neighbor) => {
+      if (!visited[neighbor]) {
+        visited[neighbor] = true
+        stack.push(neighbor)
+      }
+    })
+  }
+  return result
+}
